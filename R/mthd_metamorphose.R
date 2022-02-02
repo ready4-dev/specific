7f208f9ed88c24f914cce7c98887d3bc11739247
Matthew Hamilton <matthew.hamilton@orygen.org.au>
2022-02-02 17:23:56 +1100

    added metamorphose mthd

--- R/mthd_metamorphose.R
+++ R/mthd_metamorphose.R
@@ -1,5 +1,34 @@
 #' 
 #' Metamorphose from one ready4 framework module (or sub-module) class to another
+#' @name metamorphose-SpecificMixed
+#' @description metamorphose method applied to SpecificMixed
+#' @param x An object of class SpecificMixed
+#' @param to_1L_chr To (a character vector of length one), Default: 'SpecificSynopsis'
+#' @param rmd_fl_nms_ls R Markdown file names (a list), Default: NULL
+#' @return Y (a ready4 S4)
+#' @rdname metamorphose-methods
+#' @aliases metamorphose,SpecificMixed-method
+#' @export 
+#' @importFrom ready4show make_rmd_fl_nms_ls
+#' @importFrom stringi stri_replace_last_regex
+#' @importFrom ready4 metamorphose
+methods::setMethod("metamorphose", "SpecificMixed", function (x, to_1L_chr = "SpecificSynopsis", rmd_fl_nms_ls = NULL) 
+{
+    if (is.null(rmd_fl_nms_ls)) {
+        rmd_fl_nms_ls <- ready4show::make_rmd_fl_nms_ls("Lngl_Mdls_HTML", 
+            pdf_fl_nm_1L_chr = "Lngl_Mdls_PDF", word_fl_nm_1L_chr = "Lngl_Mdls_Word")
+    }
+    if (to_1L_chr == "SpecificSynopsis") {
+        y_r4 <- SpecificSynopsis(b_SpecificResults = x@c_SpecificResults)
+        y_r4 <- renewSlot(y_r4, "a_Ready4showPaths@outp_data_dir_1L_chr", 
+            x@b_SpecificParameters@paths_ls$output_data_dir_1L_chr %>% 
+                stringi::stri_replace_last_regex("/Output", ""))
+        y_r4 <- renewSlot(y_r4, "rmd_fl_nms_ls", rmd_fl_nms_ls)
+    }
+    return(y_r4)
+})
+#' 
+#' Metamorphose from one ready4 framework module (or sub-module) class to another
 #' @name metamorphose-SpecificConverter
 #' @description metamorphose method applied to SpecificConverter
 #' @param x An object of class SpecificConverter
--- data-raw/s4_fns/metamorphose.R
+++ data-raw/s4_fns/metamorphose.R
@@ -8,3 +8,23 @@ metamorphose_SpecificConverter <- function(x,
                                      paths_chr = paths_chr)
   return(x_SpecificModels)
 }
+metamorphose_SpecificMixed <- function(x,
+                                       to_1L_chr = "SpecificSynopsis",
+                                       rmd_fl_nms_ls = NULL){
+  if(is.null(rmd_fl_nms_ls)){
+    rmd_fl_nms_ls <- ready4show::make_rmd_fl_nms_ls("Lngl_Mdls_HTML",
+                                                    pdf_fl_nm_1L_chr = "Lngl_Mdls_PDF",
+                                                    word_fl_nm_1L_chr = "Lngl_Mdls_Word")
+  }
+  if(to_1L_chr == "SpecificSynopsis"){
+    y_r4 <- SpecificSynopsis(b_SpecificResults = x@c_SpecificResults)
+    y_r4 <- renewSlot(y_r4,
+                      "a_Ready4showPaths@outp_data_dir_1L_chr",
+                      x@b_SpecificParameters@paths_ls$output_data_dir_1L_chr %>% stringi::stri_replace_last_regex("/Output",""))#X@paths_chr
+    y_r4 <- renewSlot(y_r4,
+                      "rmd_fl_nms_ls",
+                      rmd_fl_nms_ls
+    )
+  }
+  return(y_r4)
+}
